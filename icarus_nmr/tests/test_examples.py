04af66e0dcd9c75788e763d4ab47c4cb3d4c4527
v.stadnytskyi@gmail.com <v.stadnytskyi@gmail.com>
2019-12-02 23:43:09 -0500

    current dir

--- icarus_nmr/tests/test_examples.py
+++ icarus_nmr/tests/test_examples.py
@@ -2,5 +2,6 @@ def test_one_plus_one_is_two():
     "Check that one and one are indeed two."
     assert 1 + 1 == 2
 def test_print_current_dir():
-    from os import getcwd
-    print(getcwd())
+    from os import getcwd, listdir
+    print('project root dir',getcwd())
+    print('project list dir',listdir())
