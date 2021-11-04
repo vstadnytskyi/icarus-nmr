7b84d8b110c1a287b1118e88c6100b41ab111fb6
Doctr (Travis CI) <drdoctr@users.noreply.github.com>
2021-11-04 15:32:38 +0000

    Update docs after building Travis build 132 of vstadnytskyi/icarus-nmr
    The docs were built from the branch 'master' against the commit
    f09fb46086034285bbad0dea1bb1c94fe570c232.
    
    The Travis build that generated this commit is at
    https://app.travis-ci.com/vstadnytskyi/icarus-nmr/jobs/546688623.
    
    The doctr command that was run is
    
        /home/travis/virtualenv/python3.7.1/bin/doctr deploy --built-docs docs/build/html .

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.0.post31+g12e4b72',
+    VERSION: '0.1.0.post32+gf09fb46',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
